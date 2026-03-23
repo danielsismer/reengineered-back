729bec14d1ed1575aa26965f4b915faa9eec7323
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-03-23 14:04:30 -0300

    JAV-11 refactor: remove lombok annotations in User entity

--- reenginered/src/main/java/com/weg/reenginered/domain/entity/User.java
+++ reenginered/src/main/java/com/weg/reenginered/domain/entity/User.java
@@ -1,11 +1,5 @@
 package com.weg.reenginered.domain.entity;
 
-import lombok.*;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class User {
 
     private Long id;
@@ -13,9 +7,52 @@ public class User {
     private String email;
     private String password;
 
+    public User(){
+
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
