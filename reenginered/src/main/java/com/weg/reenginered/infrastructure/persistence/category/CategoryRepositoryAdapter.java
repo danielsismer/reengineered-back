155a10caa58bbeba3fcad0c3f9471dc1f0f281fd
Hugo <hugo_paim@estudante.sesisenai.org.br>
2026-04-09 13:10:36 -0300

    JAV-14 refactor: change logic save Category repository

--- reenginered/src/main/java/com/weg/reenginered/infrastructure/persistence/category/CategoryRepositoryAdapter.java
+++ reenginered/src/main/java/com/weg/reenginered/infrastructure/persistence/category/CategoryRepositoryAdapter.java
@@ -1,11 +1,14 @@
 package com.weg.reenginered.infrastructure.persistence.category;
 
 import com.weg.reenginered.application.mapper.category.CategoryMapper;
+import com.weg.reenginered.application.mapper.product.ProductMapper;
 import com.weg.reenginered.domain.dto.filter.CategoryFilter;
 import com.weg.reenginered.domain.entity.Category;
+import com.weg.reenginered.domain.entity.Product;
 import com.weg.reenginered.domain.exception.category.CategoryNotFound;
 import com.weg.reenginered.domain.port.CategoryPort;
 import com.weg.reenginered.infrastructure.persistence.category.spec.CategorySpec;
+import com.weg.reenginered.infrastructure.persistence.product.ProductJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,10 +20,13 @@ public class CategoryRepositoryAdapter implements CategoryPort {
 
     private final CategoryJpaRepository repository;
     private final CategoryMapper categoryMapper;
+    private final ProductJpaRepository productRepository;
+    private final ProductMapper productMapper;
 
     @Override
     public Category save(Category category) {
-        return categoryMapper.toEntity(repository.save(categoryMapper.toJpa(category)));
+        List<Product> products = category.getProducts();
+        return categoryMapper.toEntity(repository.save(categoryMapper.toJpa(category)),products);
     }
 
     @Override
--- reenginered/src/main/java/com/weg/reenginered/infrastructure/persistence/product/ProductJpaRepository.java
+++ reenginered/src/main/java/com/weg/reenginered/infrastructure/persistence/product/ProductJpaRepository.java
@@ -1,11 +1,13 @@
 package com.weg.reenginered.infrastructure.persistence.product;
 
+import com.weg.reenginered.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,5 +15,8 @@ public interface ProductJpaRepository extends JpaRepository<ProductJpa, Long>, J
 
     @Query("SELECT p FROM ProductJpa p JOIN FETCH p.category WHERE p.id = :id")
     Optional<ProductJpa> findByIdWithCategory(@Param("id") Long id);
+
+    @Query("SELECT p FROM ProductJpa p JOIN FETCH p.category WHERE p.category = :category")
+    List<ProductJpa> findByCategory(@Param("category") Category category);
 
 }
